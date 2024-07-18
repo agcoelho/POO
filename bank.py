e053a3a1a954d305927a4748144573a53a9816cc
agcoelho <afonsogcoelho@gmail.com>
2024-07-18 15:33:37 -0400

    adicionando o modo transferencia

--- bank.py
+++ bank.py
@@ -1,2 +1,10 @@
 import core
 
+account = core.Acc(1, 'baby', 2500, 6000, 0)
+account2 = core.Acc(2, 'lice', 4000, 1200, 0)
+
+
+
+account.transfer(account2)
+
+
--- core.py
+++ core.py
@@ -1,23 +1,49 @@
-class acc:
-    def __init__(self, number, name, balance, limit):
-        self.__number = number
+class Acc:
+    def __init__(self, id, name, balance, limit, value):
+        self.__id = id
         self.__name = name
         self.__balance = balance
         self.__limit = limit
+        self.value = value
 
     def extract(self):
         print('You have ${:.2f} available in your account for withdraw and ${:.2f} available in credit'.format(self.__balance, self.__limit))
 
     def withdraw(self):
-        value = float(input('How much do you wish do withdraw?\n'))
+        self.value = float(input('How much do you wish do withdraw?\n'))
 
-        self.__balance -= value
+        self.__balance -= self.value
 
         print('You still have ${:.2f} available'.format(self.__balance))
 
     def deposit(self):
-        value = float(input('how much do you wish to deposit?\n'))
+        self.value = float(input('how much do you wish to deposit?\n'))
 
-        self.__balance += value
+        self.__balance += self.value
 
         print('you now have {:.2f} available in your account'.format(self.__balance))
+
+    def withdraw2(self):
+        self.balance -= self.value
+        
+
+    def deposit2(self):
+        self.__balance += self.value
+
+        
+
+    def transfer(self, destination):
+        option = 0
+        while option != 1:
+            self.value = float(input('how much do you wish to transfer?\n'))
+
+            print('transfer {:.2f} to {}'.format(self.value, destination))
+
+            option = int(input('Is everything correct?\n1-YES\n2-NO\n'))
+            if option == 1:
+                self.__balance -= self.value
+                destination.__balance += self.value
+                print('congratulations! everything went well, your current balance is: {:.2f}'.format(self.__balance))
+            
+            
+                                
