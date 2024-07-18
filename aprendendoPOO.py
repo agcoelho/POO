4b4f1aa222f738707cbe9f0b3e1fc3f59d2ed886
agcoelho <afonsogcoelho@gmail.com>
2024-07-18 13:00:19 -0400

    fazendo operações de caixa

--- aprendendoPOO.py
+++
@@ -1,5 +0,0 @@
-
-
-def create_acc(number, owner, balance, limit):
-    acc = {'number': number, 'owner': owner, 'balance': balance, 'limit': limit}
-    return acc
--- bank.py
+++ bank.py
@@ -1,5 +1,19 @@
-from aprendendoPOO import create_acc
+import functions
 
-acc = {12, 'baby', 1600, 6000}
+acc = functions.create_acc(12, 'baby', 1600, 6000)
 
 print(acc)
+
+
+deposit = float(input('how much do you wish to deposit?\n'))
+
+functions.deposit(acc, deposit)
+
+functions.extract(acc)
+
+withdraw = float(input('how much do you wish to withdraw?\n'))
+
+functions.withdraw(acc, withdraw)
+
+functions.extract(acc)
+
